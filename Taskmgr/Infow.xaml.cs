72f24248ea940ad022fb608b982d355430692318
CortCart <75949309+CortCart@users.noreply.github.com>
2021-01-28 16:59:42 +0200

    Add project files.

---
+++ Taskmgr/Infow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Taskmgr
+{
+    /// <summary>
+    /// Interaction logic for Infow.xaml
+    /// </summary>
+    public partial class Infow : Window
+    {
+       
+        public  Infow()
+        {
+            InitializeComponent();
+
+           
+          
+        }
+
+        private void Id_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Id_Loaded(object sender, RoutedEventArgs e)
+        {
+            Process[] processes = Process.GetProcesses();
+            foreach (var proc in processes)
+            {
+                if (proc.ProcessName.ToString() == Name)
+                {
+                    Id.Text = $"Id:{proc.Id.ToString()}";
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.name = Id.Text;
+            this.Close();
+        }
+    }
+}
---
+++ Taskmgr/MainWindow.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.VisualBasic;
+
+namespace Taskmgr
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static  string name;
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            {
+                Process[] processes = Process.GetProcesses();
+                for (int i = 0; i < 100000; i++)
+                {
+                    Task.Items.Add(" ");
+                }
+
+
+                for (int i = 0; i < processes.Count(); i++)
+                {
+                    Task.Items[i] = processes[i].ProcessName.ToString();
+                }
+            }
+        }
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Process[] processes = Process.GetProcesses();
+            foreach (var proces in processes)
+            {
+                Task.Items.Add(proces);
+            }
+        }
+
+        private void Task_MouseMove(object sender, MouseEventArgs e)
+        {
+           
+          
+        }
+
+        private void GetInfo_Click(object sender, RoutedEventArgs e)
+        {
+            Process[] processes = Process.GetProcesses();
+            foreach (var proces in processes)
+            {
+                if (Task.SelectedItem.ToString() == proces.ProcessName.ToString())
+                {
+
+                    try
+                    {
+                        string info = string.Empty;
+                        info += $"ID :{proces.Id.ToString()}";
+                        info += $"\nStart time : {proces.StartTime.ToString()}";
+                        MessageBox.Show(info);
+                        break;
+                    }
+                    catch (Exception)
+                    {
+
+                        MessageBox.Show("Access denied");
+                    }
+                    
+                }
+                    
+            }           
+           for (int i = 0; i < processes.Count(); i++)
+            {
+                Task.Items[i] = processes[i].ProcessName.ToString();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Process[] processes = Process.GetProcesses();
+            foreach (var proces in processes)
+            {
+                if (Task.SelectedItem.ToString() == proces.ProcessName.ToString())
+                {
+                    try
+                    {
+                        proces.Kill();
+                        break;
+                    }
+                    catch (Exception)
+                    {
+
+                        MessageBox.Show("Not select task");
+                    }
+                   
+                }
+
+            }
+            for (int i = 0; i < processes.Count(); i++)
+            {
+                Task.Items[i] = processes[i].ProcessName.ToString();
+            }         
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            Infow infow = new Infow();
+            infow.ShowDialog();
+            try
+            {
+                if (name==null)
+                {
+                   
+                }
+                else
+                {
+                    Process.Start(name);
+                }
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Unvalid app");
+            }
+            Process[] processes = Process.GetProcesses();           
+           for (int i = 0; i < processes.Count(); i++)
+            {
+                Task.Items[i] = processes[i].ProcessName.ToString();
+            }
+
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            Process[] processes = Process.GetProcesses();          
+            for (int i = 0; i < processes.Count(); i++)
+            {
+                Task.Items[i] = processes[i].ProcessName.ToString();
+            }
+        }
+    }
+}
